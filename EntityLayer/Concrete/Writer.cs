094aafa5228c2639097ae03313786a7780ac8e51
beyzauyanksoy <70273889+beyzauyanksoy@users.noreply.github.com>
2022-01-29 20:12:13 +0300

    İlişkiler ve context sınıfı oluşturuldu, webconfig yapılandıırlması ve ayarları yapıldı.Migrations ve veri tabanı oluşturuldu.

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,31 @@ namespace EntityLayer.Concrete
 {
    public class Writer
     {
+        [Key]
         public int WriterID { get; set; }
-        public int WriterName { get; set; }
-        public int WriterSurname { get; set; }
-        public int WriterImage { get; set; }
-        public int WriterMail { get; set; }
-        public int WriterPassword { get; set; }
+
+        [StringLength(50)]
+        public string WriterName { get; set; }
+
+        [StringLength(50)]
+        public string WriterSurname { get; set; }
+
+        [StringLength(100)]
+        public string WriterImage { get; set; }
+
+
+        [StringLength(50)]
+        public string WriterMail { get; set; }
+
+
+        [StringLength(20)]
+        public string WriterPassword { get; set; }
+
+
+        //heading ilişkisi
+        public ICollection<Heading> Headings { get; set; }
+
+        //content ilişkisi
+        public ICollection<Content> Contents { get; set; }
     }
 }
