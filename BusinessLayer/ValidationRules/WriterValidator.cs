3f883dbdade646ca4c392bbb2c9e26adb9833e6d
Beyza Uyanıksoy <beyzauyanik57@hotmail.com>
2022-03-10 00:22:27 +0300

    Additions have been made to the project.

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class WriterValidator: AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar Adını Boş Geçemezsiniz.");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar soyadını Boş Geçemezsiniz.");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkımda kısmını Boş Geçemezsiniz.");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Unvan kısmını Boş Geçemezsiniz.");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapınız.");
+            RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Lütfen 50 karakterden fazla değer girişi yapmayınız.");
+
+
+        }
+    } 
+}
